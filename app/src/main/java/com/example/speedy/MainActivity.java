f5846d9012dfa5ab2da7c4c407a104d530829bd7
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-07-13 10:12:27 +0100

    Create About and Profile activities

--- app/src/main/java/com/example/speedy/MainActivity.java
+++ app/src/main/java/com/example/speedy/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.speedy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //UI elements
+        Button aboutButton = findViewById(R.id.btn_about_alc);
+        Button profileButton = findViewById(R.id.btn_my_profile);
+
+        aboutButton.setOnClickListener(v -> gotoAboutPage());
+        profileButton.setOnClickListener(v -> gotoProfilePage());
+
+    }
+
+    private void gotoAboutPage() {
+        Intent aboutIntent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(aboutIntent);
+        startActivity(aboutIntent);
+    }
+
+    private void gotoProfilePage() {
+        Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(profileIntent);
+        startActivity(profileIntent);
     }
 }
