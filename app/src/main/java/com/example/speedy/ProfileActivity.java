671cb034d51cba45213443523504977bfc539ae6
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-07-15 02:29:53 +0100

    Display profile

--- app/src/main/java/com/example/speedy/ProfileActivity.java
+++ app/src/main/java/com/example/speedy/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.example.speedy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Display back button
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        //Set title
+        this.setTitle("My Profile");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
